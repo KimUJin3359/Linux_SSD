2726645eb8aa8c6b1d1d2be453a46ecffb7655e3
KimUJin3359 <wj3359@naver.com>
2021-03-25 16:22:19 +0900

    v1.3_update_testcase2

---
+++ srcs/testapp2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Step1. exec: write 0~5 0xAAAABBBB -> 30 times
+// Step2. exec: write 0~5 0x12345678 -> 1 time
+// Step3. read right?
+
+int main()
+{
+	FILE *fd;
+	char cmd[128];
+
+	// ssd will make a result.txt
+	// Step1. write 30 times
+	for (int i = 0; i < 30; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			sprintf(cmd, "./ssd write %d 0xAAAABBBB", j);
+			system(cmd);
+		}
+	}
+	// Step2. write 1 time
+	for (int i = 0; i < 5; i++)
+	{
+		sprintf(cmd, "./ssd write %d 0x12345678", i);
+		system(cmd);
+	}
+	// Step3. read 0~5
+	for (int i = 0; i < 5; i++)
+	{
+		// read ith address
+		sprintf(cmd, "./ssd read %d", i);
+		system(cmd);
+
+		// diff result.txt and test2.txt
+		system("diff ./test_result/test2.txt ./result.txt > ./test_result/result2.txt");
+		
+		// open result2.txt
+		fd = fopen("./test_result/result2.txt", "r");
+		if (fd != NULL)
+		{
+			printf("# CAN'T READ THE RESULT TEST FILE #\n");
+			return (0);
+		}
+
+		// fail
+		if (!(fgetc(fd) && feof(fd)))
+		{
+			printf("===================================\n");
+			printf("# SORRY YOUR SSD FAIL TEST CASE 2 #\n");	
+			printf("===================================\n");
+			fclose(fd);
+			system("rm ./test_result/result1.txt");
+			return (0);
+		}
+	}
+	// success
+	printf("======================================\n");
+	printf("# CONGRATULATION SUCCESS TEST CASE 1 #\n");	
+	printf("======================================\n");
+	fclose(fd);
+	system("rm ./test_result/result1.txt");
+	return (0);
+}
